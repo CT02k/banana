3917bfbe2b49745bece211d81a48e4092196ecb2
CT02k <joaopaulocfwork@gmail.com>
2026-03-13 22:41:04 -0300

    feat: add error handling utilities and middleware

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/health", (_req, res) => {
+app.get("/health", async (_req, res) => {
+    const receivedDate = new Date().getMilliseconds();
+
     res.status(200).json({
-        ok: true
-    });
+        ok: true,
+        ping: `${new Date().getMilliseconds() - receivedDate}ms`
+    });    
 });
 
 export default app;
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../shared/utils/error";
+
+export function errorHandler(
+    error: unknown,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+        error: error.name,
+        message: error.message,
+        details: error.details ?? null
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    error: "InternalServerError",
+    message: "Unexpected internal error"
+  })
+}
