bcc4d7b2e908dec0d80d54a6db236dcb877a0ae6
CT02k <joaopaulocfwork@gmail.com>
2026-03-13 21:52:27 -0300

    feat: add basic express server

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const required = (variable: string): string => {
+    if (!process.env[variable]) throw new Error(`${variable} is required, fill the .env`);
+    return process.env[variable]
+}
+
+const env = {
+  PORT: process.env.PORT ?? "3000",
+};
+
+export default env;
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("hello world")
+import app from "./app";
+import env from "./config";
+
+app.listen(env.PORT)
